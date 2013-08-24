5b8999369f93f9f6879a09c34a4d9c5d8c712580
Mariusz Kulerski <mariusz.kulerski@millwardbrown.com>
2013-08-24 15:29:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheWall::Application.config.secret_token = '98c1470b45e4127f388ac9c241d3ccc377274baef77dde911520e6a18d877bf6490f83090d572d7f7de9b90dc9954458f6c84f08e4ea40727e191aaa2db32606'
